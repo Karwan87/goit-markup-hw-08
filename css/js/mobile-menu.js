223d68d376139d0ff5f8ed300cd91e7443df0bdf
Karwan87 <113824233+Karwan87@users.noreply.github.com>
2022-12-11 19:19:17 +0100

    Add files via upload

--- css/js/mobile-menu.js
+++ css/js/mobile-menu.js
@@ -1,5 +1,5 @@
 const menuOverlay = document.querySelector(".mobile-menu");
-const closeMenuButton = document.querySelector(".mobile-menu__close-button");
+const closeMenuButton = document.querySelector(".mobile-menu__button--x");
 const openMenuBtn = document.querySelector(".mobile-menu__button");
 
 openMenuBtn.addEventListener("click", (e) => {
